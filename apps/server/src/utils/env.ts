98921d099193609d52229e0272836f316b64bcfd
raineworks <mail@raineworks.com>
2024-08-05 00:55:45 +0000

    fix: fixed the local storage driver

--- apps/server/src/utils/env.ts
+++ apps/server/src/utils/env.ts
@@ -2,9 +2,10 @@ import { object, string, z } from 'zod';
 
 const EnvSchema = object({
 	NODE_ENV: z.enum(['development', 'test', 'production']),
-	PORT: string(),
+	PORT: string().optional(),
 	TURBO_TOKEN: z.string(),
-	TZ: z.string()
+	TZ: z.string(),
+	LOCAL_STORAGE_DIR: z.string().optional()
 });
 
 export const env = EnvSchema.parse(Bun.env);
--- apps/server/src/utils/storage.ts
+++ apps/server/src/utils/storage.ts
@@ -1,16 +1,20 @@
-import { mkdir } from 'node:fs/promises';
+import fs from 'fs';
+import path from 'path';
 
-export const initLocalStorage = async (dir: string) => {
-	await mkdir(dir), { recursive: true };
+export const initLocalStorage = (dirPath: string) => {
+	const basePath = path.resolve(dirPath);
+	if (!fs.existsSync(basePath)) {
+		fs.mkdirSync(basePath, { recursive: true });
+	}
 	return {
 		writeFile: async (path: string, data: ReadableStream) => {
-			await Bun.write(`${dir}/${path}`, new Response(data));
+			await Bun.write(`${basePath}/${path}`, new Response(data));
 		},
 		streamFile: (path: string) => {
-			return Bun.file(`${dir}/${path}`).stream();
+			return Bun.file(`${basePath}/${path}`).stream();
 		},
 		exists: async (path: string) => {
-			return await Bun.file(`${dir}/${path}`).exists();
+			return await Bun.file(`${basePath}/${path}`).exists();
 		}
 	};
 };
