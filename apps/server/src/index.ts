6ff3c673487f24a2e73bd74527f4e1eab2e8a1e1
raineworks <mail@raineworks.com>
2024-08-05 00:30:39 +0000

    feat: adding local storage driver

---
+++ apps/server/src/index.ts
@@ -0,0 +1,28 @@
+import { Hono } from 'hono';
+import { auth } from '@/middleware/auth';
+import { env } from '@/utils/env';
+import { zValidator } from '@hono/zod-validator';
+import { z } from 'zod';
+import artifacts from '@/routes/artifacts/index';
+
+const app = new Hono()
+	.basePath('/v8')
+	.use(
+		'/*',
+		zValidator(
+			'header',
+			z.object({
+				authorization: z.string().refine((value) => value.startsWith('Bearer '), {
+					message: 'Invalid Authorization header.'
+				})
+			})
+		),
+		auth
+	)
+	.use('/*', zValidator('query', z.object({ teamId: z.string() }).or(z.object({ slug: z.string() }))))
+	.route('/artifacts', artifacts);
+
+export default {
+	port: env.PORT ?? 8000,
+	fetch: app.fetch
+};
